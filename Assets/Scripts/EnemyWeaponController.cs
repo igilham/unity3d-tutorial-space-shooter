1e548538bc7c5ff01d2fbecb4b0c32633c40d541
Ian Gilham <ian.gilham@gmail.com>
2015-12-07 21:57:14 +0000

    Added basic enemy ship

---
+++ Assets/Scripts/EnemyWeaponController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWeaponController : MonoBehaviour
+{
+
+    public float fireRate;
+    public float fireDelay;
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        InvokeRepeating("Fire", fireDelay, fireRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Fire()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.Play();
+    }
+}
