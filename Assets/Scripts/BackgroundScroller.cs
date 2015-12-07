b0e167f65463f8d68e14c678e64f6eb87b097ef2
Ian Gilham <ian.gilham@gmail.com>
2015-12-07 21:13:24 +0000

    Implemented scrolling background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    public float scrollSpeed;
+    public float tileLength;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        var newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileLength);
+        transform.position = startPosition + Vector3.forward * newPosition;
+    }
+}
